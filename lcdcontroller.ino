9dd058c1722033745f5634cb26c4e0c477f2a667
Plasma <56369290+PlasmaAsDev@users.noreply.github.com>
2022-01-19 17:54:46 -0300

    Fast lcd goes brrrr

--- lcdcontroller.ino
+++ lcdcontroller.ino
@@ -9,6 +9,13 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7); //Initialize the lcd class
 void setup() {
   lcd.begin(16, 2); //Beginning the lcd, 16x2
   Serial.begin(9600); //Beginning the Serial
+  Serial.setTimeout(200);
+  
+  lcd.setCursor(0,0);
+  lcd.print("Lon = ");
+  lcd.setCursor(0,1);
+  lcd.print("Lat = ");
+
   Serial.println("Arduino started, you have to send data with the script specified in the repository.");
   Serial.println("https://github.com/PlasmaAsDev/lcd_iss_tracker");
 }
@@ -16,18 +23,23 @@ void setup() {
 
 void loop() {
   while(!Serial.available());
-  lcd.clear();
+
+  lcd.setCursor(6,0);
+  lcd.print("                        ");
+  lcd.setCursor(6,1);
+  lcd.print("                        ");
+
 
   String incommingStr = Serial.readStringUntil('\n'); //Read the complete string
+  String lon = incommingStr.substring(0,7); //Divide the received string in the first 7 characters
 
-  String lon = incommingStr.substring(0, 16); //Divide the received string in the first 16 characters
+  String lat = incommingStr.substring(7);
 
-  String lat = incommingStr.substring(16);
 
-  lcd.setCursor(0,0); 
+  lcd.setCursor(6,0); 
   lcd.print(lon); // Print the longitude value on lcd
 
-  lcd.setCursor(0,1);
+  lcd.setCursor(6,1);
   lcd.print(lat); // Print the latitude value on lcd
 
   delay(500);
