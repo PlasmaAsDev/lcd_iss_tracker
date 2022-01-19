ed66de17c2a925e641d83a0d7f803efffc14a788
Plasma <56369290+PlasmaAsDev@users.noreply.github.com>
2022-01-19 17:56:54 -0300

    Location sent faster

--- main.py
+++ main.py
@@ -6,21 +6,21 @@ import json
 url_iss = "http://api.open-notify.org/iss-now.json" # Api to get location
 
 
-ser = serial.Serial('replace-with-your-com-port', 9600, timeout=0.050) # replace "replace-with-your-com-port" with your com port 
+ser = serial.Serial("replace-with-your-com-port", 9600, timeout=0.050) # replace "replace-with-your-com-port" with your com port 
 
 def get_iss_location(): # getting and parsing data
     response = requests.get(url=url_iss)
     obj = json.loads(response.content)
-    lat = f"lat = {float(obj['iss_position']['latitude'])}" 
-    lon = f"lon = {float(obj['iss_position']['longitude'])}"
+    lat = f"{float(obj["iss_position"]["latitude"])}" 
+    lon = f"{float(obj["iss_position"]["longitude"])}"
 
-    while len(lat) < 16:
+    while len(lat) < 7:
         lat = lat + " "
 
-    while len(lon) < 16:
+    while len(lon) < 7:
         lon = lon + " "
 
-    final_location = f"{lon}{lat}"
+    final_location = f"{lon[:7]}{lat}"
     return final_location.encode()
 
 
@@ -29,5 +29,4 @@ if __name__ == "__main__":
         location = get_iss_location()
         ser.write(location)
         print("Data sent")
-        time.sleep(5) #Update the location every 5 seconds
-
+        time.sleep(1) #Update the location every 1 second
